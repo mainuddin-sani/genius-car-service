197df541b7e35405995ea2cd33bd6517d2658398
mainuddin-sani <main12sani@gmail.com>
2022-06-10 01:02:09 +0600

    fake data api call

--- src/pages/Home/Expert/Expert.js
+++ src/pages/Home/Expert/Expert.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Card, Col } from "react-bootstrap";
 
-const Expert = () => {
-    return (
-        <div>
-            <h1>Experts</h1>
-        </div>
-    );
+const Expert = ({experts}) => {
+    const {name, img, Job_title} = experts;
+    console.log(experts.img)
+  return (
+    <Col md={4}>
+      <Card>
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <p>{Job_title}</p>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
 };
 
 export default Expert;
--- src/pages/Home/Expertes/Expertes.js
+++ src/pages/Home/Expertes/Expertes.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import Expert from "../Expert/Expert";
+import expert1 from "../../../images/experts/expert-1.jpg";
+import expert2 from "../../../images/experts/expert-2.jpg";
+import expert3 from "../../../images/experts/expert-3.jpg";
+import expert4 from "../../../images/experts/expert-4.jpg";
+import expert5 from "../../../images/experts/expert-5.jpg";
+import expert6 from "../../../images/experts/expert-6.png";
 
 const Expertes = () => {
-    return (
-        <div>
-            <h1>Experts</h1>
-        </div>
-    );
+  const experts = [
+    { id: 1, name: "Mainuddin Sani", Job_title: "Front End Developer", img: expert1},
+    { id: 1, name: "Alex Sani", Job_title: "Backend Developer", img: expert2 },
+    { id: 1, name: "Alex", Job_title: "Software Developer", img: expert3 },
+    { id: 1, name: "Jhon Doe", Job_title: "Software Engineer", img: expert4 },
+    { id: 1, name: "Malex", Job_title: "Google Software Engineer", img: expert5 },
+    { id: 1, name: "Salex", Job_title: "Microsoft Software Engineer", img: expert6 },
+  ];
+  return (
+    <section className="pb-5">
+      <Container>
+        <h3 className="pb-4 text-center">Our expert Team</h3>
+        <Row className="g-4">
+          {experts.map((experts) => (
+            <Expert key={experts.id} experts={experts}></Expert>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Expertes;
